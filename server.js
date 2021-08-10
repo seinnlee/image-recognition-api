97362428f67809819a1872be931375589f6a4e35
sein <44532313+seinnlee@users.noreply.github.com>
2021-08-10 15:19:19 -0700

    adding port

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)  });
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => { console.log(`App is running on port ${PORT}`); });
+app.listen(PORT || 3000, () => { console.log(`App is running on port ${PORT}`); });
 
 
 
