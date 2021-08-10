c6765989e547f34f3d140fc5f54c80b4afa0d0b7
sein <44532313+seinnlee@users.noreply.github.com>
2021-08-10 15:11:34 -0700

    environmental variable PORT created

--- server.js
+++ server.js
@@ -33,10 +33,11 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)  });
 
+const PORT = process.env.PORT;
+app.listen(PORT, () => { console.log(`App is running on port ${PORT}`); });
 
-app.listen(3000, () => { console.log("app is running on port 3000"); });
 
 
 /*
